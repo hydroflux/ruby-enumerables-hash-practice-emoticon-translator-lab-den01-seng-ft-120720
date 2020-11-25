19d3be833ee6a4f007e281a9e87632f91750a3ba
Jack Hubert <jphubert93@gmail.com>
2020-11-25 19:17:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,7 @@ end
 
 def get_japanese_emoticon(file, english_emoticon)
   # code goes here
-  emotion = YAML.load_file(file).key(english_emoticon)
-  library[emotion][1]
+  library = load_library(file)
 end
 
 def get_english_meaning(emoticon)
